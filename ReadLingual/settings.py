4fea69f617516ce3781cbab2d5ace630e00b40fa
Kociub <jakub.kociubinski@gmail.com>
2013-10-06 17:10:41 +0200

    Update settings.py
    Deleted user specific settings

--- ReadLingual/settings.py
+++ ReadLingual/settings.py
@@ -4,7 +4,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    ('root', 'jakub.kociubinski@gmail.com'),
+    ('root', 'root@readlingual.com'),
 )
 
 MANAGERS = ADMINS
@@ -15,7 +15,7 @@ DATABASES = {
         'NAME': 'ReadLingual',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': '!QAZ2wsx',
+        'PASSWORD': 'passwd',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
