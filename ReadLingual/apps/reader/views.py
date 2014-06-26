72d8a8f05b7a462eeb2fb163f86d834386fbbcc3
Kociub <jakub.kociubinski@gmail.com>
2014-06-27 01:19:39 +0200

    Added index number to Chapter Chapter text from chapter split with splitlines Added wordlookup javascript

--- ReadLingual/apps/reader/views.py
+++ ReadLingual/apps/reader/views.py
@@ -7,7 +7,7 @@ def chapter(request, chapteroriginal_id, chapter_id):
     try:
         chapter = Chapter.objects.get(pk=chapter_id)
         chapteroriginal = Chapter.objects.get(pk=chapteroriginal_id)
-        chapter_paragraphs = zip(chapteroriginal.text.split('\n'), chapter.text.split('\n'))
+        chapter_paragraphs = zip(filter(None, chapteroriginal.text.splitlines()), filter(None, chapter.text.splitlines()))
     except Chapter.DoesNotExist:
         raise Http404
     return render(request, 'reader/chapter.html', {'chapter_paragraphs': chapter_paragraphs, 'chapteroriginal': chapteroriginal, 'chapter': chapter})
