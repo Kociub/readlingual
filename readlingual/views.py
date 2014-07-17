73581538e6c886fe25eb5f28a68dd347fa42b67d
Kociub <jakub.kociubinski@gmail.com>
2014-07-17 22:58:18 +0200

    redirecting search queries to google for now

--- readlingual/views.py
+++ readlingual/views.py
@@ -1,14 +1,9 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, HttpResponseRedirect
 from web_search import google
 
 def search(request):
-        params = {}
         query = request.REQUEST.get("query", "")
         if query not in [None, '']:
-            params['query'] = query
-            search_blocking = google(query, 10)
-            params['result'] = search_blocking
-            for (name, url, desc) in search_blocking:
-                print name, url
-        return render_to_response('search.html', params) 
+            return HttpResponseRedirect("https://www.google.com/search?q="+query+"+site:readlingual.com");
+        return render_to_response('search.html') 
         
